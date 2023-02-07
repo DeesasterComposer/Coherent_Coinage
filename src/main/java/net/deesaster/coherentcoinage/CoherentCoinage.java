ebce8d0c81477721c47b9a75aa9fb9e53b759362
DeesasterComposer <boondinisdipperis@gmail.com>
2023-02-07 16:22:58 -0600

    Initial commit

---
+++ src/main/java/net/deesaster/coherentcoinage/CoherentCoinage.java
@@ -0,0 +1,15 @@
+package net.deesaster.coherentcoinage;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CoherentCoinage implements ModInitializer {
+	public static final String MOD_ID = "coherentcoinage";
+	public static final Logger LOGGER = LoggerFactory.getLogger("coherentcoinage");
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/deesaster/coherentcoinage/CoherentCoinageClient.java
@@ -0,0 +1,11 @@
+package net.deesaster.coherentcoinage;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CoherentCoinageClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
